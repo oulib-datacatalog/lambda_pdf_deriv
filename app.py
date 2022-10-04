16d91cb9a44541392c3da1dd15ca485a3d0a6b74
Tyler Pearson <tdpearson@ou.edu>
2022-10-04 12:14:09 -0500

    use aws power tools for logging, restructuring, increase memory consts, additional error handling

--- app.py
+++ app.py
@@ -1,42 +1,68 @@
 import io
-import logging
 import traceback
 
 from json import dumps, loads
 from os import getenv
 from pathlib import Path
-from typing import Iterator, BinaryIO
+from typing import Iterator, BinaryIO, List
 
 import boto3
 from botocore.exceptions import ClientError
-from chalice import Chalice, NotFoundError
+from chalice import Chalice, NotFoundError, BadRequestError
+from chalice.app import ConvertToMiddleware
+from aws_lambda_powertools import Logger
+from aws_lambda_powertools import Tracer
+
 from PIL import Image
+
+
+#########################################################
+# Default values
 
 DEFAULT_IMAGE_SCALE = 0.4  # of original size
 DEFAULT_IMAGE_EXTENSIONS = ('jpg', 'tif', 'tiff', 'png')
 
 SOURCE_BAG_LOCATIONS = ['source', 'private/external-preservation', 'private/preservation', 'private/private', 'private/shareok']
 
-LAMBDA_MAX_MEMORY_FOR_PDF = 2147483648  # 2GB
+LAMBDA_MAX_MEMORY_FOR_PDF = 4294967296  # 4GB
+LAMBDA_MAX_MEMORY_FOR_DERIV = 2147483648  # 2GB
+
+
+########################################################
+# Configurations from environment
 
 SQS_QUEUE_DERIV = getenv("SQS_QUEUE_DERIV")
 SQS_QUEUE_PDF = getenv("SQS_QUEUE_PDF")
 S3_BUCKET = getenv("S3_BUCKET")
 
+
+########################################################
+# Chalice application settings
+
 app = Chalice(app_name='lambda_pdf_deriv')
 app.debug = True
-app.log.setLevel(logging.WARN)
+
+logger = Logger()  # See .chalice/config.json to change LOG_LEVEL
+tracer = Tracer()
+app.register_middleware(ConvertToMiddleware(logger.inject_lambda_context))
+app.register_middleware(ConvertToMiddleware(tracer.capture_lambda_handler))
+
+########################################################
+# AWS bindings
 
 s3_client = boto3.client('s3')
 s3_paginator = s3_client.get_paginator('list_objects_v2')
-sqs = boto3.resource('sqs')
 
+sqs = boto3.resource('sqs')
 deriv_queue = sqs.get_queue_by_name(QueueName=SQS_QUEUE_DERIV)
 pdf_queue = sqs.get_queue_by_name(QueueName=SQS_QUEUE_PDF)
 
 
+########################################################
+# Helper functions
+
 def _filter_keep(file: str, extensions: tuple[str, ...] = DEFAULT_IMAGE_EXTENSIONS, ignore_orig: bool = True) -> bool:
-    """ filters to apply to a file's name to determine to keep """
+    """ filters to apply to a file's name to determine if to keep """
     if not file.endswith(extensions):
         return False
     if ignore_orig and 'orig' in Path(file).name.lower():
@@ -47,24 +73,23 @@ def _filter_keep(file: str, extensions: tuple[str, ...] = DEFAULT_IMAGE_EXTENSIO
 
 
 def _images(prefix: str, extensions: tuple[str, ...] = DEFAULT_IMAGE_EXTENSIONS, ignore_orig: bool = True) -> Iterator[dict]:
-    """ yield images in S3 with filtering including file size """
+    """ yield image paths and file sizes in S3 with applied filtering """
     for page in s3_paginator.paginate(Bucket=S3_BUCKET, Prefix=prefix):
         try:
             contents = page['Contents']
         except KeyError as e:
-            app.log.error(f'Failed to access: {e}')
+            logger.error(f'Failed to access: {e}')
             raise NotFoundError('Could not find bag matching request!')
         for obj in contents:
             try:
                 file = obj['Key']
                 size = obj['Size']
             except KeyError as e:
-                app.log.error(f'Failed to access: {e}')
+                logger.error(f'Failed to access: {e}')
                 raise NotFoundError('Could not find images matching request!')
 
             if _filter_keep(file, extensions=extensions, ignore_orig=ignore_orig):
                 yield {'file': file, 'size': size}
-
 
 
 def _find_source_bag(bag: str) -> str:
@@ -78,6 +103,114 @@ def _find_source_bag(bag: str) -> str:
             continue  # try next location
     raise NotFoundError('Could not find bag matching request!')
 
+
+def _s3_byte_stream(bucket: str, key: str) -> BinaryIO:
+    """ return an S3 object's data as a BytesIO stream """
+    return io.BytesIO(s3_client.get_object(Bucket=bucket, Key=key)['Body'].read())
+
+
+def _object_size(bucket: str, key: str) -> int:
+    """ return the file size of an S3 object in bytes """
+    return s3_client.head_object(Bucket=bucket, Key=key)['ContentLength']
+
+
+def _is_file_too_large(file_sizes: List[int], max_size: int = LAMBDA_MAX_MEMORY_FOR_DERIV, buffer_ratio: float = 0.3) -> bool:
+    """
+    compute if enough memory available based on memory size and with a buffer ratio reserved for derivatives
+    defaults to a memory size for derivative generation and a reservation of 30% of available memory
+    """
+    return max_size * (1 - buffer_ratio) - sum(file_sizes) < 0
+
+
+def _generate_pdf(bag: str, title: str = None, author: str = None, subject: str = None, keywords: str = None) -> dict:
+    """ Generates PDF from default derivative images """
+    destination = f'derivative/{bag}/pdf/{bag}.pdf'
+
+    try:  # Test for existing pdf
+        s3_client.head_object(Bucket=S3_BUCKET, Key=destination)
+        logger.info(f'PDF already exists: {destination}')
+        return {'message': 'PDF already exists'}
+    except ClientError:
+        pass  # does not exist - continue
+
+    try:  # Test for existing derivatives
+        image_paths, image_sizes = zip(
+            *((item['file'], item['size'])
+              for item in images_derivative(bag, scale=DEFAULT_IMAGE_SCALE)
+              )
+        )
+    except NotFoundError:
+        logger.error('Missing derivative - failed to generate PDF')
+        return {'message': 'missing derivative to generate PDF'}
+
+    # Test total size of derivatives is under half of allocated memory size
+    logger.debug(f'Total size of derivatives: {sum(image_sizes)}')
+    logger.debug(f'LAMBDA_MAX_MEMORY_FOR_PDF: {LAMBDA_MAX_MEMORY_FOR_PDF}')
+    if _is_file_too_large(image_sizes, max_size=LAMBDA_MAX_MEMORY_FOR_PDF, buffer_ratio=0.3):
+        logger.error(f'Total size of derivatives is more than half of available memory: {sum(image_sizes)}')
+        return {'message': 'memory limit exceeded'}
+
+    # get contents of first image to bootstrap PDF generation
+    image_path = image_paths[0]
+    pdf = Image.open(_s3_byte_stream(bucket=S3_BUCKET, key=image_path))
+
+    # save generated PDF back to S3
+    pdf_file = io.BytesIO()
+    pdf.save(
+        pdf_file,
+        format='PDF',
+        save_all=True,
+        title=title,
+        author=author,
+        subject=subject,
+        keywords=keywords,
+        append_images=(
+            Image.open(_s3_byte_stream(bucket=S3_BUCKET, key=image_path))
+            for image_path in image_paths[1:]  # skipping first image used to bootstrap pdf
+        )
+    )
+    pdf_file.flush()
+    pdf_file.seek(0)
+    try:
+        s3_client.put_object(Bucket=S3_BUCKET, Key=destination, Body=pdf_file)
+    except:
+        logger.error(f'Failed to save PDF to S3 for bag: {bag}')
+    logger.info(f'Generated PDF for bag: {bag}')
+    return {'message': 'success'}
+
+
+########################################################
+# Event handlers
+
+@app.on_sqs_message(queue=SQS_QUEUE_PDF, batch_size=1)
+def pdf_generator(event) -> None:
+    """ Watch for messages to generate PDF """
+    for record in event:
+        record_body = loads(record.body)
+        logger.debug(record_body)
+        try:
+            _generate_pdf(**record_body)
+        except (NotFoundError, BadRequestError) as e:
+            logger.error(f"Failed to generate PDF: {record_body['bag']}")
+        logger.debug(f"Created PDF: {record_body['bag']}")
+
+
+@app.on_sqs_message(queue=SQS_QUEUE_DERIV, batch_size=1)
+def deriv_generator(event) -> None:
+    """ watch for messages to generate an image derivative """
+    for record in event:
+        record_body = record.body
+        logger.debug(record_body)
+        bag, scale, image, location = loads(record_body)
+        try:
+            resize_individual(bag, scale, image, location)
+        except (NotFoundError, BadRequestError) as e:
+            logger.error(f"Failed to resize: {bag}, {image}")
+        logger.debug(f"Resized: {bag}, {image} -> {scale}, {location}")
+
+
+########################################################
+# Exposed functions
 
 @app.route('/find/{bag}')
 def find_source_bag(bag: str) -> str:
@@ -111,85 +244,13 @@ def available_derivatives(bag: str) -> list[str]:
     )
 
 
-def _s3_byte_stream(bucket: str, key: str) -> BinaryIO:
-    """ return an S3 object's data as a BytesIO stream """
-    return io.BytesIO(s3_client.get_object(Bucket=bucket, Key=key)['Body'].read())
-
-
-def _generate_pdf(bag: str, title: str = None, author: str = None, subject: str = None, keywords: str = None) -> dict:
-    """ Generates PDF from default derivative images """
-    destination = f'derivative/{bag}/pdf/{bag}.pdf'
-
-    try:  # Test for existing pdf
-        s3_client.head_object(Bucket=S3_BUCKET, Key=destination)
-        app.log.info(f'PDF already exists: {destination}')
-        return {'message': 'PDF already exists'}
-    except ClientError:
-        pass  # does not exist - continue
-
-    try:  # Test for existing derivatives
-        image_paths, image_sizes = zip(
-            *((item['file'], item['size'])
-              for item in images_derivative(bag, scale=DEFAULT_IMAGE_SCALE)
-              )
-        )
-    except NotFoundError:
-        app.log.error('Missing derivative - failed to generate PDF')
-        return {'message': 'missing derivative to generate PDF'}
-
-    # Test total size of derivatives is under half of allocated memory size
-    total_deriv_size = sum(image_sizes)
-    app.log.debug(f'Total size of derivatives: {total_deriv_size}')
-    app.log.debug(f'LAMBDA_MAX_MEMORY_FOR_PDF: {LAMBDA_MAX_MEMORY_FOR_PDF}')
-    if total_deriv_size > LAMBDA_MAX_MEMORY_FOR_PDF / 2:  # TODO: test memory usage to confirm setting
-        app.log.error(f'Total size of derivatives is more than half of available memory: {total_deriv_size}')
-        return {'message': 'memory limit exceeded'}
-
-    # get contents of first image to bootstrap PDF generation
-    image_path = image_paths[0]
-    pdf = Image.open(_s3_byte_stream(bucket=S3_BUCKET, key=image_path))
-
-    # save generated PDF back to S3
-    pdf_file = io.BytesIO()
-    pdf.save(
-        pdf_file,
-        format='PDF',
-        save_all=True,
-        title=title,
-        author=author,
-        subject=subject,
-        keywords=keywords,
-        append_images=(
-            Image.open(_s3_byte_stream(bucket=S3_BUCKET, key=image_path))
-            for image_path in image_paths[1:]  # skipping first image used to bootstrap pdf
-        )
-    )
-    pdf_file.flush()
-    pdf_file.seek(0)
-    try:
-        s3_client.put_object(Bucket=S3_BUCKET, Key=destination, Body=pdf_file)
-    except:
-        app.log.error(f'Failed to save PDF to S3 for bag: {bag}')
-    app.log.info(f'Generated PDF for bag: {bag}')
-    return {'message': 'success'}
-
-
-@app.on_sqs_message(queue=SQS_QUEUE_PDF, batch_size=1)
-def pdf_generator(event) -> None:
-    """ Watch for messages to generate PDF """
-    for record in event:
-        record_body = loads(record.body)
-        app.log.debug(record_body)
-        _generate_pdf(**record_body)
-
-
 @app.route('/pdf/{bag}', methods=['GET', 'POST'])
 def generate_pdf(bag: str) -> dict:
     """ API endpoint for requesting PDF generation """
     request = app.current_request
     data = request.json_body if request.json_body else {}
-    app.log.debug(f'Using queue: {pdf_queue}')
-    app.log.info(f'Processing {bag}')
+    logger.debug(f'Using queue: {pdf_queue}')
+    logger.info(f'Processing {bag}')
     resp = pdf_queue.send_message(
         MessageBody=dumps(
             {
@@ -201,7 +262,7 @@ def generate_pdf(bag: str) -> dict:
             }
         )
     )
-    app.log.debug(resp)
+    logger.debug(resp)
     return {'message': 'submitted for processing'}
 
 
@@ -213,17 +274,20 @@ def resize_individual(bag: str, scale: float, image_path: str, location: str = "
 
     try:  # Test for existing derivative
         s3_client.head_object(Bucket=S3_BUCKET, Key=destination)
-        app.log.info(f'Derivative already exists: {destination}')
+        logger.info(f'Derivative already exists: {destination}')
         return {'message': 'image already exists'}
     except ClientError:
         pass  # does not exist - continue
 
+    bag_location = location if location else bag
+    if _is_file_too_large(_object_size(S3_BUCKET, f'{bag_location}/data/{image_path}')):
+        raise BadRequestError("The source image is too large")
+
     try:
-        bag_location = location if location else bag
         source_image = Image.open(_s3_byte_stream(S3_BUCKET, f'{bag_location}/data/{image_path}'))
     except Exception as e:
-        app.log.error(f'Failed to open source image: {image_path}')
-        app.log.error(traceback.format_exc())
+        logger.error(f'Failed to open source image: {image_path}')
+        logger.error(traceback.format_exc())
         return {'message': 'error opening source image'}
 
     size = (x * float(scale) for x in source_image.size)
@@ -235,33 +299,27 @@ def resize_individual(bag: str, scale: float, image_path: str, location: str = "
     image_file.seek(0)
 
     s3_client.put_object(Bucket=S3_BUCKET, Key=destination, Body=image_file)
-    app.log.info(f'Created S3 object: {destination}')
+    logger.info(f'Created S3 object: {destination}')
     return {'message': 'created resized image'}
-
-
-@app.on_sqs_message(queue=SQS_QUEUE_DERIV, batch_size=1)
-def deriv_generator(event) -> None:
-    """ watch for messages to generate an image derivative """
-    for record in event:
-        record_body = record.body
-        app.log.debug(record_body)
-        bag, scale, image, location = loads(record_body)
-        resize_individual(bag, scale, image, location)
 
 
 @app.route('/resize/{bag}/{scale}')
 def resize(bag: str, scale: float) -> dict:
     """ API endpoint to resize images for specified bag """
-    app.log.debug(f'Using queue: {deriv_queue}')
-    app.log.info(f'Processing {bag}')
+    logger.debug(f'Using queue: {deriv_queue}')
+    logger.info(f'Processing {bag}')
     location = _find_source_bag(bag)['location']
-    app.log.info(f'Using location {location}')
-    for full_s3_key in images_source(bag, location):
-        image = full_s3_key.get("file").split('/')[-1]
-        resp = deriv_queue.send_message(
-            MessageBody=dumps((bag, scale, image, location))
-        )
-        app.log.debug(resp)
+    logger.info(f'Using location {location}')
+    for image_details in images_source(bag, location):
+        size = image_details.get('size')
+        image_filename = image_details.get("file").split('/')[-1]
+        if _is_file_too_large([size]):
+            logger.error(f"{image_filename} from {bag} is too large to process!")
+        else:
+            resp = deriv_queue.send_message(
+                MessageBody=dumps((bag, scale, image_filename, location))
+            )
+            logger.debug(resp)
     return {'message': 'submitted for processing'}
 
 
